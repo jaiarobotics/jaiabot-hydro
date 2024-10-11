06edbb4a0e13834448781e97d8a783a594be0ec2
Nick Marshall <nickjrmarshall@gmail.com>
2024-10-11 10:01:41 -0400

    Beginning of custom Jaia calibration procedure in cal script

--- src/python/atlas_scientific_ezo_ec/calibrate_atlas_oem.py
+++ src/python/atlas_scientific_ezo_ec/calibrate_atlas_oem.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import atlas_oem
+import datetime
 import time
 
 probe = atlas_oem.AtlasOEM()
@@ -17,6 +18,11 @@ def presentMenu(menu):
     done = False
     while not done:
         clearScreen()
+        print(datetime.datetime.now())
+        probe.dump()
+        print()
+        print()
+        print()
         print(menu['title'])
         print('============')
         print()
@@ -39,6 +45,18 @@ def presentMenu(menu):
             print('Invalid choice.')
             input()
 
+def pollEC():
+    print('Getting 10 seconds of data...')
+    ec_old = None
+    for i in range(0, 10):
+        ec = probe.EC()
+        if ec_old:
+            delta_percent = abs(ec - ec_old) / ec_old * 100
+        else:
+            delta_percent = 0.0
+        print(f'time: {datetime.datetime.now()}  EC: {ec: 6.0f}  delta: {delta_percent: 3.2f}%')
+        ec_old = ec
+        time.sleep(1)
 
 def setProbeType():
     value = input('Enter probe type value (K value) > ')
@@ -48,9 +66,8 @@ def setProbeType():
     except ValueError:
         input('K value must be a number [press enter to continue]')
 
-
 def setTemperatureCompensation():
-    value = input('Enter temperature (deg C) > ')
+    value = input('Enter temperature of solution (deg C) > ')
 
     try:
         probe.setTemperatureCompensation(float(value))
@@ -61,6 +78,7 @@ def setTemperatureCompensation():
 def printProbeStatus():
     try:
         clearScreen()
+        print(datetime.datetime.now())
         probe.dump()
         input('Press enter to continue')
     except Exception as e:
@@ -87,7 +105,7 @@ def doCalibration(description: str, type: int):
                 delta_percent = abs(ec - ec_old) / ec_old * 100
             else:
                 delta_percent = 0.0
-            print(f'EC: {ec: 6.2f}  delta: {delta_percent: 3.1f}%')
+            print(f'time:{datetime.datetime.now()}  EC: {ec: 6.0f}  delta: {delta_percent: 3.2f}%')
             ec_old = ec
             time.sleep(1)
         if input('Calibrate now (Y/n)?').lower() in ['', 'y']:
@@ -100,6 +118,54 @@ def doCalibration(description: str, type: int):
     except ValueError:
         input('Value must be a number.  Press enter.')
 
+def doJaiaCalibration(description: str, type: int, value: int):
+    while True: 
+        print(f"Running {description} calibration at {value} μS/cm.")
+        ec_old = None
+        delta_list = []
+        for i in range(0,10):
+            ec = probe.EC()
+            if ec_old:
+                delta_percent = abs(ec - ec_old) / ec_old * 100
+            else:
+                delta_percent = 0.0
+            delta_list.append(delta_percent)
+            ec_old = ec
+        
+        if delta_list.max() < 0.10:
+            break
+    
+    input(f"{description} calibration at {value} μS/cm complete. Press enter")
+    probe.setCalibration(value)
+    probe.setCalibrationRequest(type)
+
+
+def jaiaCalibration():
+    # Give countdown for user to prepare for calibration to begin
+    countdown = 5
+    while countdown != -1:
+        print(f"Starting calibration procedure... {countdown}")
+        countdown -= 1
+        time.sleep(1)
+        clearScreen()
+
+    # Start from a fresh calibration state
+    clearCalibration()
+
+    # Set the probe type to the Jaia default
+    # This must be changed if the probe ever changes from K 1.0
+    probe.setProbeType(1.0)
+
+    # Get temperature of solution from user
+    setTemperatureCompensation()
+
+    # Ensure probe is dry
+    print("To begin calibrating, the probe must be completely dry.")
+    time.sleep(1)
+    input("Once you've ensured the probe is completely dry, press enter.")
+
+    doJaiaCalibration()
+
 
 def dryCalibration():
     doCalibration('DRY', 2)
@@ -111,9 +177,11 @@ def singlePointCalibration():
 
 def highCalibration():
     doCalibration('DUAL POINT HIGH', 5)
+    doCalibration('DUAL POINT HIGH', 5)
 
 
 def lowCalibration():
+    doCalibration('DUAL POINT LOW', 4)
     doCalibration('DUAL POINT LOW', 4)
 
 
@@ -127,6 +195,11 @@ def calibrate():
                 'func': clearCalibration
             },
             {
+                'description': 'Jaia Calibration',
+                'key': 'j',
+                'func': jaiaCalibration
+            },
+            {
                 'description': 'Dry Calibration',
                 'key': 'd',
                 'func': dryCalibration
@@ -137,14 +210,14 @@ def calibrate():
                 'func': singlePointCalibration
             },
             {
-                'description': 'Dual Point High Calibration',
-                'key': 'h',
-                'func': highCalibration
-            },
-            {
                 'description': 'Dual Point Low Calibration',
                 'key': 'l',
                 'func': lowCalibration
+            },
+            {
+                'description': 'Dual Point High Calibration',
+                'key': 'h',
+                'func': highCalibration
             },
             {
                 'description': 'Exit Menu',
@@ -161,12 +234,24 @@ presentMenu({
         {
             'description': 'Print probe status',
             'key': 's',
+            'key': 's',
             'func': printProbeStatus
+        },
+        {
+            'description': 'Poll EC for 10 seconds',
+            'key': 'l',
+            'func': pollEC
         },
         {
             'description': 'Set probe type',
             'key': 'p',
+            'key': 'p',
             'func': setProbeType
+        },
+        {
+            'description': 'Set temperature compensation',
+            'key': 't',
+            'func': setTemperatureCompensation
         },
         {
             'description': 'Set temperature compensation',
