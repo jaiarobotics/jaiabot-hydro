44f1e5fc14fad71bea84b5074dc1d995fbb60bfe
Toby Schneider <toby@gobysoft.org>
2024-11-11 17:00:25 -0500

    Fix failure in first-boot.sh by running apt in noninteractive mode and forcing jaiabot-embedded to take precedence to anything on the existing filesystem

--- rootfs/customization/includes.chroot/etc/jaiabot/init/first-boot.sh
+++ rootfs/customization/includes.chroot/etc/jaiabot/init/first-boot.sh
@@ -181,13 +181,13 @@ if [[ "$do_install" = "true" ]]; then
        echo "$jaia_embedded_debconf" | debconf-set-selections -
        debconf-get-selections | grep jaia
    fi
+   export DEBIAN_FRONTEND=noninteractive
    if dpkg -s jaiabot-embedded; then
        # if it's already installed, reconfigure
-       export DEBIAN_FRONTEND=noninteractive
        dpkg-reconfigure jaiabot-embedded;
    else
        # otherwise install it
-       apt install -y /opt/jaiabot-embedded*.deb;
+       apt install -y -o Dpkg::Options::="--force-confnew" /opt/jaiabot-embedded*.deb;
    fi
 fi
    
