cad7da7e6459dbfccbe26b44c32f1c8a5a507549
JRoybot <jeff@roy.net>
2024-11-12 08:57:20 -0500

    Add rerender commands to test to emulate real dom behavior

--- src/web/jcc/client/components/TaskSettingsPanel/__tests__/TaskSettingsPanel.test.tsx
+++ src/web/jcc/client/components/TaskSettingsPanel/__tests__/TaskSettingsPanel.test.tsx
@@ -6,8 +6,10 @@ import { MissionTask, TaskType, DiveParameters, DriftParameters } from "../../sh
 
 import { log } from "console";
 
-const mockMinimumProps: Props = {
+let mockProps: Props = {
+    isEditMode: true,
     enableEcho: false,
+    onChange: (task?: MissionTask) => mockOnChangeCheckParameters(task),
 };
 
 const sendEventToParentWindowMock = jest.fn(() => {
@@ -42,77 +44,13 @@ const mockOnChangeCheckParameters = (task?: MissionTask) => {
             //TODO
             break;
     }
+    mockProps.task = task;
     sendEventToParentWindowMock();
-};
-const mockDriftParameters: DriftParameters = {
-    //Mock Drift Parameters
-    drift_time: 0,
-};
-
-//Non-Bottom Dive Prop Setup
-const nonBottomDiveParameters: DiveParameters = {
-    //Mock Non-Bottom Dive Parameters
-    max_depth: 30,
-    depth_interval: 10,
-    hold_time: 2,
-    bottom_dive: false,
-};
-const mockNonBottomDiveTask: MissionTask = {
-    //Mock Non-Bottom Dive Task
-    type: TaskType.DIVE,
-    dive: nonBottomDiveParameters,
-    surface_drift: mockDriftParameters,
-};
-const mockNonBottomDiveProps: Props = {
-    //Mock Non-Bottom Dive Props
-    task: mockNonBottomDiveTask,
-    isEditMode: true,
-    enableEcho: false,
-    onChange: (task?: MissionTask) => mockOnChangeCheckParameters(task),
-};
-
-//Bottom Dive Prop Setup
-const bottomDiveParameters: DiveParameters = {
-    //Mock Bottom Dive Parameters
-    bottom_dive: true,
-};
-const mockBottomDiveTask: MissionTask = {
-    //Mock Bottom Dive Task
-    type: TaskType.DIVE,
-    dive: bottomDiveParameters,
-    surface_drift: mockDriftParameters,
-};
-const mockBottomDiveProps: Props = {
-    //Mock Bottom Dive Props
-    task: mockBottomDiveTask,
-    isEditMode: true,
-    enableEcho: false,
-    onChange: (task?: MissionTask) => mockOnChangeCheckParameters(task),
-};
-
-//Bad Bottom Dive Prop Setup
-const badBottomDiveParameters: DiveParameters = {
-    //Mock ill-formed Bottom Dive Parameters
-    max_depth: 30, //these 3 parameters should not be presetn for Bottom Dives
-    depth_interval: 10,
-    hold_time: 2,
-    bottom_dive: true,
-};
-const mockBadBottomDiveTask: MissionTask = {
-    //Mock Bottom Dive Task
-    type: TaskType.DIVE,
-    dive: badBottomDiveParameters,
-    surface_drift: mockDriftParameters,
-};
-const mockProps: Props = {
-    isEditMode: true,
-    enableEcho: false,
-    onChange: (task?: MissionTask) => mockOnChangeCheckParameters(task),
 };
 
 describe("MUI Select Component Examples", () => {
     test("Get by Test ID", async () => {
-        render(<TaskSettingsPanel {...mockProps} />);
+        const { rerender } = render(<TaskSettingsPanel {...mockProps} />);
 
         // Get the Select Component
         const selectElement = screen.getByTestId("task-select-input-id");
@@ -124,27 +62,40 @@ describe("MUI Select Component Examples", () => {
         // Change the selection to Dive
         await userEvent.selectOptions(selectElement, "DIVE");
 
+        // rerender with updated props
+        rerender(<TaskSettingsPanel {...mockProps} />);
+
         // Verify that the selected value is Dive
         expect((selectElement as HTMLSelectElement).value).toBe("DIVE");
 
         // Change the selection to Dive
         await userEvent.selectOptions(selectElement, "SURFACE_DRIFT");
 
+        // rerender with updated props
+        rerender(<TaskSettingsPanel {...mockProps} />);
+
         // Verify that the selected value is Dive
         expect((selectElement as HTMLSelectElement).value).toBe("SURFACE_DRIFT");
 
-          // Change the selection to Dive
-          await userEvent.selectOptions(selectElement, "STATION_KEEP");
+        // rerender with updated props
+        rerender(<TaskSettingsPanel {...mockProps} />);
 
-          // Verify that the selected value is Dive
-          expect((selectElement as HTMLSelectElement).value).toBe("STATION_KEEP");
-  
-         // Change the selection to Dive
-         await userEvent.selectOptions(selectElement, "CONSTANT_HEADING");
+        // Change the selection to Dive
+        await userEvent.selectOptions(selectElement, "STATION_KEEP");
 
-         // Verify that the selected value is Dive
-         expect((selectElement as HTMLSelectElement).value).toBe("CONSTANT_HEADING");
- 
-        
-   });
+        // rerender with updated props
+        rerender(<TaskSettingsPanel {...mockProps} />);
+
+        // Verify that the selected value is Dive
+        expect((selectElement as HTMLSelectElement).value).toBe("STATION_KEEP");
+
+        // Change the selection to Dive
+        await userEvent.selectOptions(selectElement, "CONSTANT_HEADING");
+
+        // rerender with updated props
+        rerender(<TaskSettingsPanel {...mockProps} />);
+
+        // Verify that the selected value is Dive
+        expect((selectElement as HTMLSelectElement).value).toBe("CONSTANT_HEADING");
+    });
 });
