32ab40fc6e9a15e12c63eceeeeb5a06bc3de49a9
JRoybot <jeff@roy.net>
2024-11-12 16:08:54 -0500

    remove debug print statements

--- src/web/jcc/client/components/TaskSettingsPanel/TaskSettingsPanel.tsx
+++ src/web/jcc/client/components/TaskSettingsPanel/TaskSettingsPanel.tsx
@@ -161,9 +161,6 @@ function TaskOptionsPanel(props: Props) {
             newTask.surface_drift["drift_time"] = surface_drift.drift_time;
         }
 
-        console.log("handleToggle newTask=");
-        console.log(newTask);
-
         props.onChange(newTask);
 
         //Save bottom dive to local storage
