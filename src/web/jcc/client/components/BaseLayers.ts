fd008b158e2cf9c372f4bc7e7b0503410be68525
michael-twomey <michael-twomey1@outlook.com>
2024-11-22 11:01:11 -0500

    Test commit

--- src/web/jcc/client/components/BaseLayers.ts
+++ src/web/jcc/client/components/BaseLayers.ts
@@ -4,7 +4,7 @@ import { XYZ, OSM } from "ol/source";
 import { persistVisibility } from "./VisibleLayerPersistance";
 import * as Layers from "./shared/Layers";
 
-// test 4
+// test 5
 export function createBaseLayerGroup() {
     const layers = [
         Layers.getArcGISSatelliteImageryLayer(),
