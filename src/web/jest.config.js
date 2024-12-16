a1abca08c63741fa6cc1cde78bddf7455c4425ba
michael-twomey <michael-twomey1@outlook.com>
2024-12-16 16:59:28 -0500

    Update path in jest config

--- src/web/jest.config.js
+++ src/web/jest.config.js
@@ -140,7 +140,7 @@ const config = {
     setupFiles: ["fake-indexeddb/auto", "<rootDir>/tests/jest.setup.js"],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
-    setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
+    setupFilesAfterEnv: ["<rootDir>/tests/setup-tests.ts"],
 
     // The number of seconds after which a test is considered as slow and reported as such in the results.
     // slowTestThreshold: 5,
