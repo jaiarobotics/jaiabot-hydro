e8d0f4a68dbc99e06e4dc52641b1625ea772a9fb
michael-twomey <michael-twomey1@outlook.com>
2025-01-14 16:38:38 -0500

    Remove sample waypoint code from Map component

--- src/web/components/Map/Map.tsx
+++ src/web/components/Map/Map.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect } from "react";
 
 import { map } from "../../openlayers/maps/map";
-import { missionLayer } from "../../openlayers/layers/mission-layer";
 
 import { Feature, MapBrowserEvent } from "ol";
-import { Coordinate } from "ol/coordinate";
-import { Point } from "ol/geom";
-import { Icon, Style } from "ol/style";
 
 import "./Map.less";
-
-const waypointSVG = require("../../style/icons/waypoint.svg");
 
 export default function Map() {
     useEffect(() => {
@@ -25,38 +19,11 @@ export default function Map() {
 
         if (feature) {
             switch (feature.getProperties().name) {
-                case "waypoint":
-                    handleWaypointClick(feature);
-                    return;
                 default:
                     return;
             }
         }
-
-        addWaypointToMap(event.coordinate);
-    };
-
-    const handleWaypointClick = (feature: Feature) => {
-        console.log("Clicked:", feature.getProperties());
     };
 
     return <div id="map"></div>;
-}
-
-function addWaypointToMap(coordinate: Coordinate) {
-    const waypoint = new Feature({
-        name: "waypoint",
-        geometry: new Point(coordinate),
-    });
-    waypoint.setStyle(generateWaypointStyle());
-    missionLayer.getSource().addFeature(waypoint);
-}
-
-function generateWaypointStyle() {
-    return new Style({
-        image: new Icon({
-            src: waypointSVG,
-            anchor: [0.5, 1],
-        }),
-    });
 }
