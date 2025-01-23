23957b3244c0f1f4a9ffbf210753be9171d2e58f
Toby Schneider <toby@gobysoft.org>
2025-01-23 16:17:31 -0500

    Allow create-virtualfleet ansible playbook to generate vfleet inventory again

--- rootfs/customization/includes.chroot/etc/jaiabot/init/jaia-create-ansible-inventory.sh
+++ rootfs/customization/includes.chroot/etc/jaiabot/init/jaia-create-ansible-inventory.sh
@@ -4,13 +4,15 @@ set -u -e
 
 # expects 'create-ansible-inventory.sh -b 1,2,3 -h 1,2' where -b is the comma separated list of bot IDs, and -h is the comma separated list of hub IDs
 # optionally takes '-f N' for fleet ID N, otherwise the fleet ID is read from debconf for jaiabot-embedded
+# also optionally takes -n for net (Defaults to 'wlan')
 
 bots=""
 hubs=""
 fleet=""
+net="wlan"
 
 # Parse command line arguments
-while getopts "b:h:f:" opt; do
+while getopts "b:h:f:n:" opt; do
     case $opt in
         b)
             bots=$OPTARG
@@ -21,12 +23,21 @@ while getopts "b:h:f:" opt; do
         f)
             fleet=$OPTARG
             ;;
+        n)
+            net=$OPTARG
+            ;;
         *)
             echo "Invalid option: -$OPTARG" >&2
             exit 1
             ;;
     esac
 done
+
+if [ "$net" = "wlan" ]; then
+    ipv="4"
+else
+    ipv="6"
+fi
 
 if [ "$fleet" = "" ]; then
     fleet=$(debconf-get-selections | grep jaiabot-embedded/fleet_id | cut -f 4)
@@ -44,7 +55,7 @@ hubs_array=($(echo "$hubs" | tr ',' ' '))
 echo "bots:"
 echo "  hosts:"
 for b in "${bots_array[@]}"; do
-    ip=$(eval jaia-ip.py --node bot --net wlan --fleet_id ${fleet} --node_id ${b} addr --ipv4)
+    ip=$(eval jaia-ip.py --node bot --net ${net} --fleet_id ${fleet} --node_id ${b} addr --ipv${ipv})
     echo "    bot${b}-fleet${fleet}:"
     echo "      ansible_user: jaia"
     echo "      ansible_host: ${ip}"
@@ -53,7 +64,7 @@ done
 echo "hubs:"
 echo "  hosts:"
 for h in "${hubs_array[@]}"; do
-    ip=$(eval jaia-ip.py --node hub --net wlan --fleet_id ${fleet} --node_id ${h} addr --ipv4)
+    ip=$(eval jaia-ip.py --node hub --net ${net} --fleet_id ${fleet} --node_id ${h} addr --ipv${ipv})
     echo "    hub${h}-fleet${fleet}:"
     echo "      ansible_user: jaia"
     echo "      ansible_host: ${ip}"
