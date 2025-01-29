e677fa0e6cb0168c9cc2e96e3501b7c0dcf65000
michael-twomey <michael-twomey1@outlook.com>
2025-01-29 09:18:50 -0500

    Update function comments in MissionAssignMenu

--- src/web/components/MissionAssignMenu/MissionAssignMenu.tsx
+++ src/web/components/MissionAssignMenu/MissionAssignMenu.tsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useContext } from "react";
-import {
-    JaiaSystemContext,
-    JaiaSystemDispatchContext,
-} from "../../context/JaiaSystem/JaiaSystemContext";
-import { JaiaSystemActions } from "../../context/JaiaSystem/jaia-system-actions";
+// React
+import React, { useContext } from "react";
 
+// MUI
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 
+// Jaia
 import Bot from "../../data/bots/bot";
 import { missionsManager } from "../../data/missions_manager/missions-manager";
+
+import {
+    JaiaSystemContext,
+    JaiaSystemDispatchContext,
+} from "../../context/JaiaSystem/JaiaSystemContext";
+import { JaiaSystemActions } from "../../context/JaiaSystem/jaia-system-actions";
 
 const UNASSIGNED_ID = -1;
 
@@ -26,7 +30,7 @@ export default function MissionAssignMenu(props: Props) {
     /**
      * Provides MenuItem value needed to display text in the Select element
      *
-     * @returns Number The MenuItem value which maps to `Bot-{BotID}` or "Unassigned"
+     * @returns {number} The MenuItem value which maps to `Bot-{BotID}` or "Unassigned"
      */
     const getMenuValue = () => {
         return missionsManager.getBot(props.missionID);
@@ -55,20 +59,19 @@ export default function MissionAssignMenu(props: Props) {
     /**
      * If a Bot is not assigned to a mission or the Bot is already assigned to this mission, display it in the dropdown menu
      *
-     * @param {Bot} bot Used to access its ID and its assigned mission ID
+     * @param {number} botID Used to access its assigned mission ID and create the MenuItem
      * @returns <MenuItem> A dropdown option for a MUI Select element
      *
      * @notes
      * If the Bot is already assigned to this mission, we need to display the Bot in the dropdown menu
-     * because the selected element needs to be an option in the menu
+     * because the selected element needs to be an option in the menu (MUI rule)
      *
      * This function is called each time the Select component calls the onChange handler
      */
-    const generateMenuItems = (bot: Bot) => {
-        const botAssignment = missionsManager.getMission(bot.getBotID());
+    const generateMenuItems = (botID: number) => {
+        const botAssignment = missionsManager.getMission(botID);
 
         if (botAssignment === UNASSIGNED_ID || botAssignment === props.missionID) {
-            const botID = bot.getBotID();
             return <MenuItem key={botID} value={botID}>{`Bot-${botID}`}</MenuItem>;
         }
     };
@@ -82,7 +85,9 @@ export default function MissionAssignMenu(props: Props) {
                 value={getMenuValue().toString()}
             >
                 <MenuItem value={UNASSIGNED_ID}>Unassigned</MenuItem>
-                {Array.from(jaiaSystemContext.bots.values()).map((bot) => generateMenuItems(bot))}
+                {Array.from(jaiaSystemContext.bots.values()).map((bot) =>
+                    generateMenuItems(bot.getBotID()),
+                )}
             </Select>
         </FormControl>
     );
