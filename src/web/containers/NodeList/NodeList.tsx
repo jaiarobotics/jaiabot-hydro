a2e239da6b83d3d36d382383781d322c536b7567
JRoybot <jeff@roy.net>
2025-02-10 09:16:25 -0500

    WIP resolving merge errors

--- src/web/containers/NodeList/NodeList.tsx
+++ src/web/containers/NodeList/NodeList.tsx
@@ -3,11 +3,12 @@ import {
     GlobalContext,
     GlobalDispatchContext,
     GlobalAction,
-    NodeType,
     GlobalContextType,
 } from "../../context/Global/GlobalContext";
 import { JaiaSystemContext } from "../../context/JaiaSystem/JaiaSystemContext";
 import { GlobalActions } from "../../context/Global/GlobalActions";
+import { jaiaGlobal } from "../../data/jaia_global/jaia-global";
+import { NodeTypes } from "../../types/jaia-system-types";
 import { HealthState } from "../../shared/JAIAProtobuf";
 import sortBy from "lodash/sortBy";
 import "./NodeList.less";
@@ -26,32 +27,33 @@ export function NodeList() {
     const bots = sortBy(Array.from(jaiaSystemContext.bots.values() ?? []), ["botID"]);
 
     /**
-     * Triggered when a node is clicked.  Dispatches GlobalAction
-     * to handle the event
+     * Triggered when a node is clicked.  Sets the selected node
+     * in jaiaGlobal and then Dispatches GlobalAction
+     * to handle the clicked node event
      *
-     * @param {NodeType} nodeType Indicates if hub or bot
+     * @param {NodeTypes} nodeType Indicates if hub or bot
      * @param {number} nodeID Hub or Bot ID
      *
      * @returns {void}
      *
      */
-    const handleClick = (nodeType: NodeType, nodeID: number) => {
+    const handleClick = (nodeType: NodeTypes, nodeID: number) => {
+        jaiaGlobal.setSelectedNode({ type: nodeType, id: nodeID });
+
         globalDispatch({
             type: GlobalActions.CLICKED_NODE,
-            nodeType: nodeType,
-            nodeID: nodeID,
         });
     };
 
     /**
      * Creates the className string for the node
      *
-     * @param {NodeType} nodeType indicates it it is a Bot or Hub
+     * @param {NodeTypes} nodeType indicates it it is a Bot or Hub
      * @param {number} nodeID ID of hub or bot
      * @param {HealthState} healthState Health of hub or bot
      * @returns {string} className
      */
-    function getClassName(nodeType: NodeType, nodeID: number, healthState: HealthState) {
+    function getClassName(nodeType: NodeTypes, nodeID: number, healthState: HealthState) {
         const faultLevel: Map<HealthState, number> = new Map([
             [HealthState.HEALTH__OK, 0],
             [HealthState.HEALTH__DEGRADED, 1],
@@ -59,7 +61,7 @@ export function NodeList() {
         ]);
         const selectedNode = globalContext.selectedNode;
         const nodeClass = "node-item";
-        const botHubClass = nodeType === NodeType.BOT ? "bot-item" : "hub-item";
+        const botHubClass = nodeType === NodeTypes.BOT ? "bot-item" : "hub-item";
         let selected = "";
         if (selectedNode.type == nodeType && selectedNode.id === nodeID) {
             selected = "selected";
@@ -73,8 +75,8 @@ export function NodeList() {
             {hubs.map((hub) => (
                 <div
                     key={`hub-${hub.getHubID()}`}
-                    onClick={() => handleClick(NodeType.HUB, hub.getHubID())}
-                    className={getClassName(NodeType.HUB, hub.getHubID(), hub.getHealthState())}
+                    onClick={() => handleClick(NodeTypes.HUB, hub.getHubID())}
+                    className={getClassName(NodeTypes.HUB, hub.getHubID(), hub.getHealthState())}
                 >
                     {"HUB"}
                 </div>
@@ -82,8 +84,8 @@ export function NodeList() {
             {bots.map((bot) => (
                 <div
                     key={`bot-${bot.getBotID()}`}
-                    onClick={() => handleClick(NodeType.BOT, bot.getBotID())}
-                    className={getClassName(NodeType.BOT, bot.getBotID(), bot.getHealthState())}
+                    onClick={() => handleClick(NodeTypes.BOT, bot.getBotID())}
+                    className={getClassName(NodeTypes.BOT, bot.getBotID(), bot.getHealthState())}
                 >
                     {bot.getBotID()}
                 </div>
